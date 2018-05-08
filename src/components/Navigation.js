2615e3d33d45f34102e353038bceac72d21302f9
Benjamin Huang <bhuang@rms.com>
2018-05-08 15:23:00 -0700

    update

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,6 +1,7 @@
 import React from 'react';
 
 import NavItem from './NavItem';
+import NavDropdown from './NavDropdown';
 
 class Navigation extends React.Component {
     render() {
@@ -19,18 +20,12 @@ class Navigation extends React.Component {
                         <NavItem path="/page2" name="Page2" />
                         <NavItem path="/page3" name="Disabled" disabled="true" />
 
-                        <li className="nav-item dropdown">
-                            <a className="nav-link dropdown-toggle" href="/" id="navbarDropdown" role="button" data-toggle="dropdown"
-                                aria-haspopup="true" aria-expanded="false">
-                                Dropdown
-              </a>
-                            <div className="dropdown-menu" aria-labelledby="navbarDropdown">
-                                <a className="dropdown-item" href="/">Action</a>
-                                <a className="dropdown-item" href="/">Another action</a>
-                                <div className="dropdown-divider"></div>
-                                <a className="dropdown-item" href="/">Something else here</a>
-                            </div>
-                        </li>
+                        <NavDropdown name="Dropdown">
+                            <a className="dropdown-item" href="/">Action</a>
+                            <a className="dropdown-item" href="/">Another action</a>
+                            <div className="dropdown-divider"></div>
+                            <a className="dropdown-item" href="/">Something else here</a>
+                        </NavDropdown>
                     </ul>
                     <form className="form-inline my-2 my-lg-0">
                         <input className="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search" />
